e34076896a8134863874bcb304aada466494fc95
delorean7182 <alikhan.talipbayev@nu.edu.kz>
2026-02-26 00:27:30 +0500

    Fix base url, changed title

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // For local development, use '/'
+  // For Plesk/production deployment, use '/'
   // For GitHub Pages deployment, change this to '/your-repo-name/'
-  base: '/Quiren-landing-page/',
+  base: '/',
 })
